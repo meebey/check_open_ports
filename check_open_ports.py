e43aa83c04fda229dd0ec1d04a425d891780e1bb
Mirco Bauer <meebey@meebey.net>
2016-09-23 11:23:11 +0800

    add GPLv3 license header

--- check_open_ports.py
+++ check_open_ports.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
+#
+# open_check_ports is a Nagios check that checks open ports against a whitelist
+#
+# Copyright (C) 2016 Mirco Bauer <meebey@meeby.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
 import nagiosplugin
